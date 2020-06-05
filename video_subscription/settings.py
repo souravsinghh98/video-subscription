1c888b550934566867f1b2286919013c146c2920
Sourav Singh <sourav.singhh98@gmail.com>
2020-06-05 17:32:36 +0530

    'second'

--- video_subscription/settings.py
+++ video_subscription/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&wvwzu&z)xqtb@nrh@urenx)_#w%#lj7t##ap%(9ktnfs65q&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['time-for-popcorn.herokuapp.com', '127.0.0.1']
 
